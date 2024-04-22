ecda5157ec400833fe369e75737c818f110de83a
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-04-22 02:02:13 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-codesniffer: 41.0.0 → 43.0.0
    * php-parallel-lint/php-parallel-lint: 1.3.2 → 1.4.0
    
    npm:
    * eslint-config-wikimedia: 0.25.0 → 0.27.0
    * grunt-banana-checker: 0.10.0 → 0.11.1
    * grunt-eslint: 24.0.1 → 24.3.0
    * grunt-stylelint: 0.18.0 → 0.19.0
    * stylelint-config-wikimedia: 0.14.0 → 0.16.1
    
    Change-Id: Ia192c122404e0bf0e9a137e7fc382047febc04dd

--- languages/ConfigManager.alias.php
+++ languages/ConfigManager.alias.php
@@ -6,11 +6,13 @@ $specialPageAliases['en'] = [
 	'ConfigManager' => [
 		'ConfigManager',
 		'Config Manager',
-] ];
+	]
+];
 
 /** German (Deutsch) */
 $specialPageAliases['de'] = [
 	'ConfigManager' => [
 		'ConfigManager',
 		'Konfigurationsverwaltung',
-] ];
+	]
+];
